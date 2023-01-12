8bdb7b7d3c0bd819aae1cd4118f353a5aa0e2689
Cinthiapiera <cinthia.maldonado@upch.pe>
2023-01-11 23:20:05 -0500

    perfil acordeon

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,19 +20,19 @@ const About = () => {
                     </div>
                 </Col>
                 <Col lg='8' md='9'>
-                    <div className="perfil__content_wrapper d-flex gap-5">
+                    <div className="perfil__content__wrapper d-flex gap-5">
                         <div className="perfil__img w-25">
                             <img src={perfil} alt="" className='w-100'/>
                         </div>
                         <div className="perfil__content w-75">
-                            <h2>Cinthia Maldonado</h2>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas amet quis ut modi commodi nemo incidunt! Maiores, voluptates? Aut doloremque ipsam nesciunt excepturi corporis atque labore. Animi optio eius nulla!</p>
+                            <h3>Cinthia Maldonado</h3>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas amet quis ut modi commodi nemo incidunt! Maiores, voluptates? Aut doloremque ipsam nesciunt excepturi corporis atque labore.</p>
                             <div className="social__links">
-                                <h6>Contactame por:</h6>
-                                <span><a href="#f"><i class="ri-facebook-circle-line"></i></a></span>
-                                <span><a href="#g"><i class="ri-github-fill"></i></a></span>
+                                <h6 className='mb-4'>Contactame por:</h6>
+                                <span><a href="#f"><i class="ri-facebook-box-fill"></i></a></span>
+                                <span><a href="#g"><i class="ri-github-line"></i></a></span>
                                 <span><a href="#l"><i class="ri-linkedin-box-fill"></i></a></span>
-                                <span><a href="#e"><i class="ri-mail-line"></i></a></span>
+                                <span><a href="#e"><i class="ri-mail-fill"></i></a></span>
                             </div>
                         </div>
                     </div>
