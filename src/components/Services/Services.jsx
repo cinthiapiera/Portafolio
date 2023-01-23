3d0f08ddff6a969eab318a760a1713f13f7e667d
Cinthiapiera <cinthia.maldonado@upch.pe>
2023-01-22 21:47:44 -0500

    servicies update

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,24 +1,63 @@
 import React from 'react';
 import {Container, Row, Col} from 'reactstrap';
 
+import './Services.css';
+
+const servicesData =[
+  {
+    icon: 'ri-code-line',
+    title: 'web Design',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae in modi hic iste nisi, molestias maiores inventore corrupti, vero, sint ad sit.',
+  },
+  {
+    icon: 'ri-code-s-slash-line',
+    title: 'web Development',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae in modi hic iste nisi, molestias maiores inventore corrupti, vero, sint ad sit.',
+  },
+  {
+    icon: 'ri-qr-code-line',
+    title: 'App Development',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae in modi hic iste nisi, molestias maiores inventore corrupti, vero, sint ad sit.',
+  },
+  {
+    icon: 'ri-search-line',
+    title: 'SEO',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae in modi hic iste nisi, molestias maiores inventore corrupti, vero, sint ad sit.',
+  },
+  {
+    icon: 'ri-landscape-line',
+    title: 'Graphics Design',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae in modi hic iste nisi, molestias maiores inventore corrupti, vero, sint ad sit.',
+  },
+  {
+    icon: 'ri-gallery-line',
+    title: 'UI/UX',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae in modi hic iste nisi, molestias maiores inventore corrupti, vero, sint ad sit.',
+  },
+  
+]
+
 const Services = () => {
   return (
     <section id="experiencia">
       <Container>
         <Row>
-          <Col lg='12'>
+          <Col lg='12' className='services__top mb-5'>
             <h6>Features</h6>
             <h2>services</h2>
           </Col>
-          <Col lg='4' md='6' sm='6'>
-            <div className="single__service">
-              <span className="service__icon">
-                <i class="ri-code-line"></i>
-              </span>
-              <h2>Diseño de Web</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ad, accusamus a, pariatur dolore iure molestias placeat magnam excepturi velit perferendis reiciendis. Saepe rerum inventore labore quidem voluptatem, aut magnam.</p>
-            </div>
-          </Col>
+            {
+              servicesData.map((item,index)=>(
+                <Col lg='4' md='6' sm='6' key={index} className='mb-4'>
+                  <div className="single__service">
+                    <span className="service__icon" >
+                      <i class={item.icon}></i></span>
+                    <h2>{item.title}</h2>
+                    <p>{item.desc}</p>
+                  </div>
+              </Col>
+              ))
+            }
         </Row>
       </Container>
     </section>
