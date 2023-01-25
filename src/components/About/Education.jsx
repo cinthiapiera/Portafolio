86fbd2b8113795d2ce3862d5766ec26078ed1cfe
Cinthiapiera <cinthia.maldonado@upch.pe>
2023-01-25 03:34:46 -0500

    education table responsive

--- src/components/About/Education.jsx
+++ src/components/About/Education.jsx
@@ -4,36 +4,36 @@ import './Education.css';
 const Education = () => {
   return (
     <table className='table'>
-        <thead>
-            <tr>
-                <th>Grado</th>
-                <th>Creditos/horas</th>
-                <th>Universidad/Instituto</th>
-                <th>Año</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>En Formación</td>
-                <td>Developer</td>
-                <td>Oracle Next Education - Alura, LATAM</td>
-                <td>2023 - presente</td>
-            </tr>
-            <tr>
-                <td>Full Stack Web Developer</td>
-                <td>700 H</td>
-                <td>Henry Bootcamp</td>
-                <td>2022 - 2023</td>
-            </tr>
-            <tr>
-                <td>Ingeniería Informática</td>
-                <td>250 C.A</td>
-                <td>Universidad Peruana Cayetano Heredia (UPCH)</td>
-                <td>2015 - 2020</td>
-            </tr>
-        </tbody>
+      <thead>
+          <tr>
+            <th>Grado</th>
+            <th>Hora/Creditos</th>
+            <th>Entidad</th>
+            <th>Año</th>
+          </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>En Formación</td>
+          <td>Developer</td>
+          <td>Oracle Next Education - Alura</td>
+          <td>2023 - Actualmente</td>
+        </tr>
+        <tr>
+          <td>Full Stack Web Developer</td>
+          <td>700 H.</td>
+          <td>Henry Bootcamp</td>
+          <td>2022 - 2023</td>
+        </tr>
+        <tr>
+          <td>Ingeniería Informática</td>
+          <td>250 C.</td>
+          <td>Universidad Peruana Cayetano Heredia</td>
+          <td>2015 - 2020</td>
+        </tr>
+      </tbody>
     </table>
   )
 }
 
-export default Education
+export default Education;
