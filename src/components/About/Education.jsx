4053bd63356c08b5b0d7c15dcf04143778eff3bd
Cinthiapiera <cinthia.maldonado@upch.pe>
2023-02-02 17:23:58 -0500

    education update

--- src/components/About/Education.jsx
+++ src/components/About/Education.jsx
@@ -10,7 +10,6 @@ const Education = () => {
               <th>Grado</th>
               <th>Tiempo</th>
               <th>Entidad</th>
-              <th>Año</th>
             </tr>
         </thead>
         <tbody>
@@ -18,19 +17,16 @@ const Education = () => {
             <td>Java Developer</td>
             <td>En Formación</td>
             <td>Oracle Next Education - Alura Latam</td>
-            <td>2023 - Presente</td>
           </tr>
           <tr>
             <td>Full Stack Web Developer</td>
-            <td>700 H.</td>
+            <td>700 Horas</td>
             <td>Henry Bootcamp</td>
-            <td>2022 - 2023</td>
           </tr>
           <tr>
             <td>Ingeniería Informática</td>
-            <td>175 C.</td>
+            <td>175 Créditos</td>
             <td>Universidad Peruana Cayetano Heredia</td>
-            <td>2015 - 2020</td>
           </tr>
         </tbody>
       </table>
